bcb591ae756ea296e70f28c6f0b595802dae8a94
IsMingo <97234500+IsMingo@users.noreply.github.com>
2022-03-15 11:34:55 +0100

    Movement3rd

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,37 +5,4 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-   //todo raycast
-   private new float speed =0.4f;
-   private void Update()
-   {
-      PlayerMouvement();
-   }
-
-
-   
-
-   private void PlayerMouvement()
-   {
-      //todo: mouvement du joueur
-      if (Input.GetKey(KeyCode.UpArrow))
-      {
-         transform.Translate(new Vector3(0f,0f,.01f*speed));
-      }
-
-      if (Input.GetKey(KeyCode.DownArrow))
-      {
-         transform.Translate(new Vector3(0f,0f,-.01f*speed));
-      }
-      
-      if (Input.GetKey(KeyCode.LeftArrow))
-      {
-         transform.Rotate(new Vector3(0f,-0.2f*speed,0f));
-      }
-      
-      if (Input.GetKey(KeyCode.RightArrow))
-      {
-         transform.Rotate(new Vector3(0,0.2f*speed,0f));
-      }
-   }
-}
+ 
